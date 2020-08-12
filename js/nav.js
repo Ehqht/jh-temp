c54c7b83517d121fabe86b1c9af594cd4796cd96
Ehqht <livia3609@gmail.com>
2020-08-12 12:55:12 +0900

    pull down navigation

---
+++ js/nav.js
@@ -0,0 +1,96 @@
+$(document).ready(function () {
+    //your code here
+  });
+
+
+$(document).on('mouseover', '.navbar-nav', function() {
+    $('.nav-sub').slideDown(300);
+});
+/*
+$(document).on('mouseover', 'header', function() {
+    $('.nav-sub').slideUp(300);
+});
+$(document).on('mouseover', '.wrap', function() {
+    $('.nav-sub').slideUp(300);
+});
+*/
+
+/*
+$(document).on('mouseover', 'div', function() {
+    if (!$(this).hasClass('navbar>container')) {
+        $('.nav-sub').slideUp(300);
+    }
+});*/
+
+
+// var zoomx = 100;
+//   $(document).ready(function() {
+//     $(".gnb").hover(
+//       function() {
+//         $(".snb").stop().slideDown(400);
+//       },
+//       function() {
+//         $(".snb").stop().slideUp(600);
+//       }
+//     );
+
+//     $(".snb").hover(
+//       function() {
+//         $(".snb").stop().slideDown(400);
+//       },
+//       function() {
+//         $(".snb").stop().slideUp(600);
+//       }
+//     );
+
+//     $(".tabBtn").hover(
+//       function() {
+
+//         $(this).children(".icon_normal").css("display","none");
+//         $(this).children(".icon_over").css("display","block");
+//       },
+//       function() {
+//         $(this).children(".icon_over").css("display","none");
+//         $(this).children(".basic").css("display","block");
+//       }
+//     );
+
+//     $(".tabBtn").click(
+//       function() {
+
+//         $("."+$(this).attr("tarx")).children(".icon_normal").addClass("basic");
+//         $("."+$(this).attr("tarx")).children(".icon_over").removeClass("basic");
+//         $(this).children(".icon_over").addClass("basic");
+//         $(this).children(".icon_normal").removeClass("basic");
+
+//         $("."+$(this).attr("tarx")).children(".icon_over").css("display","none");
+//         $("."+$(this).attr("tarx")).children(".icon_normal").css("display","none");
+//         $("."+$(this).attr("tarx")).children(".basic").css("display","block");
+
+
+//         $("."+$(this).attr("tarc")).css("display","none");
+//         $("#"+$(this).attr("tar")).css("display","block");
+
+//         $("#"+$(this).attr("tarmc")).attr("href", $(this).attr("tarm"));
+
+//       }
+//     );
+
+// //class="tabBtn tabBtngroupA" tar="lastA_1" tarc="lastA_1" tarx="tabBtngroupA"
+//   });
+// function body_zoomin() {
+//   zoomx = zoomx + 10;
+//   $("body").css("zoom",zoomx + "%");
+// }
+// function body_zoomout() {
+//   zoomx = zoomx - 10;
+//   $("body").css("zoom",zoomx + "%");
+// }
+// function body_zoomdef() {
+//   zoomx = 100;
+//   $("body").css("zoom",zoomx + "%");
+// }
+// function newWin(url,w,h) {
+// window.open
+// (url,'new_win','width='+w+',height='+h+',toolbars=no,menubars=no,scrollbars=auto');
+// }
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 // Check for empty fields
-if(empty($_POST['title'])      ||
-   empty($_POST['name'])      ||
+if(empty($_POST['name'])      ||
+   empty($_POST['phone'])      ||
    empty($_POST['email'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
@@ -10,17 +10,21 @@ if(empty($_POST['title'])      ||
    return false;
    }
 
-   
 $title = strip_tags(htmlspecialchars($_POST['title']));
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'livia3609@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form: $title ($name)";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: livia3609@naver.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_subject = "새로운 문의사항이 있습니다. 제목:$phone 작성자: $name";
+$email_body = "주흥환경 홈페이지에 새로운 문의사항이 등록되었습니다.\n\n"."세부사항은 다음과 같습니다.\n\n제목: $phone\n\n작성자: $name\n\n작성자 이메일: $email_address\n\n\n\n내용:\n$message
+
+
+
+주흥환경 웹사이트: http://www.jh-entech.co.kr";
+$headers = "From: juheong-1@hanmail.net\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
