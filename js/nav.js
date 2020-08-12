7c340420732d6ba782992bbbd44b6e12311c36c2
Ehqht <livia3609@gmail.com>
2020-08-12 17:16:21 +0900

    pull down navi on index.html

--- js/nav.js
+++ js/nav.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
 $(document).on('mouseover', '.navbar-nav', function() {
     $('.nav-sub').slideDown(300);
 });
-/*
+
 $(document).on('mouseover', 'header', function() {
     $('.nav-sub').slideUp(300);
 });
 $(document).on('mouseover', '.wrap', function() {
     $('.nav-sub').slideUp(300);
 });
-*/
+
 
 /*
 $(document).on('mouseover', 'div', function() {
