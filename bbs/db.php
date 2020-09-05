9e4daf3e2fd1fff60111fed2ed3e97e2091a928a
Ehqht <livia3609@gmail.com>
2020-09-05 14:21:22 +0900

    contact_me.js file modifying

---
+++ bbs/db.php
@@ -0,0 +1,5 @@
+<?
+    $conn = mysql_connect("localhost", "root", "tksgkcjstk");
+    mysql_select_db("bbs",$conn);
+?>
+
--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>정상적으로 문의가 접수되었습니다. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("죄송합니다. 현재 문의서버가 정상 동작하지 않습니다. 나중에 다시 시도해주세요."));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
