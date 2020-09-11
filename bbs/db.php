92e7a9e5f479f835d5846b2e8323374574dd9c0b
Ehqht <livia3609@gmail.com>
2020-09-11 15:59:16 +0900

    business portfolio modification

--- bbs/db.php
+++
@@ -1,5 +0,0 @@
-<?
-    $conn = mysql_connect("localhost", "root", "tksgkcjstk");
-    mysql_select_db("bbs",$conn);
-?>
-
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,6 @@
-<?php
+ <?php
+
+// phone == title
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['phone'])      ||
@@ -10,25 +12,19 @@ if(empty($_POST['name'])      ||
    return false;
    }
 
-//$title = strip_tags(htmlspecialchars($_POST['title']));
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'juheong-1@hanmail.net'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "새로운 문의사항이 있습니다. 제목:$phone 작성자: $name";
+
+// 보낼 Email 양식 설정 
+//$to = 'juheong-1@hanmail.net'; 
+$to = 'juheong-1@hanmail.net'; 
+$email_subject = "홈페이지에 새로운 문의사항이 등록되었습니다.. 제목:$phone, 작성자: $name";
 $email_subject = "=?UTF-8?B?".base64_encode($email_subject)."?=";
-$email_body = "주흥환경 홈페이지에 새로운 문의사항이 등록되었습니다.\n\n"."\n\n제목: $phone\n\n작성자: $name\n\n작성자 이메일: $email_address\n\n\n\n내용:\n$message
-
-
-
+$email_body = "주흥환경 홈페이지에 새로운 문의사항이 등록되었습니다.\n\n"."\n\n제목: $phone\n\n작성자: $name\n\n작성자 이메일: $email_address\n\n\n\n내용:\n$message \n\n
 주흥환경 웹사이트: http://www.jh-entech.co.kr";
 
 
 $headers = "From: juheong\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
+
 return true;         
 ?>
