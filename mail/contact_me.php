bee456a6b9271c43f2d4386a78ef0005edc9c9c5
Ehqht <livia3609@gmail.com>
2020-10-04 16:23:59 +0900

    main design done

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,8 +11,9 @@ require './SMTP.php';
 $mail = new PHPMailer(true);
 $mail->IsSMTP();
 
+
 if(empty($_POST['name'])      ||
-   empty($_POST['phone'])      ||
+   empty($_POST['title'])      ||
    empty($_POST['email'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
@@ -20,6 +21,11 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
+
+$title = $_POST['title'];
+$name =$_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
 try {
     $mail->CharSet = "utf-8";   //한글이 안깨지게 CharSet 설정
@@ -32,10 +38,9 @@ try {
     $mail->Password = 'ab640210'; // 패스워드
     $mail->SetFrom('juheong-1@hanmail.net', '주흥환경 홈페이지'); // 보내는 사람 email 주소와 표시될 이름 (표시될 이름은 생략가능)
     $mail->AddAddress('juheong-1@hanmail.net');  // 받을 사람 email 주소와 표시될 이름 (표시될 이름은 생략가능)
-    $mail->Subject = "홈페이지에 새로운 문의사항이 등록되었습니다. 제목:$phone, 작성자: $name"; // 메일 제목
+    $mail->Subject = "홈페이지에 새로운 문의사항이 등록되었습니다. 제목:$title, 작성자: $name"; // 메일 제목
     $mail->Body =
-    "주흥환경 홈페이지에 새로운 문의사항이 등록되었습니다.\n\n\n\n
-    제목: $phone\n\n작성자: $name\n\n작성자 이메일: $email\n\n\n\n내용:\n$message 
+    "주흥환경 홈페이지에 새로운 문의사항이 등록되었습니다.\n\n\n\n제목: $title\n\n작성자: $name\n\n작성자 이메일: $email\n\n\n\n내용:\n$message 
     \n\n주흥환경 웹사이트: http://www.jh-entech.co.kr"; // 내용
 
     $mail->Send(); // 발송
@@ -49,36 +54,4 @@ try {
         echo $e->getMessage(); //Boring error messages from anything else!
     }
 
-/*
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-
-// phone == title
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['phone'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-
-// 보낼 Email 양식 설정 
-//$to = 'juheong-1@hanmail.net'; 
-$to = 'juheong-1@hanmail.net'; 
-$email_subject = "홈페이지에 새로운 문의사항이 등록되었습니다.. 제목:$phone, 작성자: $name";
-$email_subject = "=?UTF-8?B?".base64_encode($email_subject)."?=";
-$email_body = "주흥환경 홈페이지에 새로운 문의사항이 등록되었습니다.\n\n"."\n\n제목: $phone\n\n작성자: $name\n\n작성자 이메일: $email\n\n\n\n내용:\n$message \n\n
-주흥환경 웹사이트: http://www.jh-entech.co.kr";
-
-
-$headers = "From: juheong\n";
-$headers .= "Reply-To: $email";   
-mail($to,$email_subject,$email_body,$headers);
-
-return true;    
-*/     
 ?>
