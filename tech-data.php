dce1f6fc78086d2f60bdba1fd0c033036e906261
Ehqht <livia3609@gmail.com>
2020-11-03 16:08:43 +0900

    customer menu title

--- tech-data.php
+++ tech-data.php
@@ -1,4 +1,4 @@
-<?php include  "./db.php"; ?>
+<?php include  "./bbs/db.php"; ?>
 
 <!DOCTYPE html>
 <html lang="ko">
@@ -10,6 +10,7 @@
   <meta name="author" content="">
 
   <title> 주흥환경 </title>
+
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -120,38 +121,130 @@
         <a href="contact.html" class="side-menu text-center">고객센터</a>
         <div class="list-group">
           <a href="tech-data.php" class="list-group-item">기술자료</a>
-
         </div>
       </div>
       <!-- Page Content -->
       <div class="container col">
   
         <!-- Page Heading/Breadcrumbs -->
-        <h1 id="sub-menu">기술자료</h1>
-  
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="index.html">Home</a>
-          </li>
-          <li class="breadcrumb-item"><a href="contact.html">고객센터</a></li>
-          <li class="breadcrumb-item"><a href="tech-data.php">기술자료</a></li>
-        </ol>
-  
-       <!-- Contact Form -->
-    <!-- In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
-    <div class="contact-content">
-      
-      
+        <div class="title-bar">
+          <div class="title-bar-h1"><h1>기술자료</h1></div>
+
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="index.html">Home</a>
+            </li>
+            <li class="breadcrumb-item"><a href="./contact.html">고객센터</a></li>
+            <li class="breadcrumb-item"><a href="./tech-data.php">기술자료</a></li>
+          </ol>
+        </div>
+
+      <div class="contact-content">
+        <table class="list-table">
+          <thead>
+          <tr>
+            <th width="70">번호</th>
+            <th width="500">제목</th>
+            <th width="120">글쓴이</th>
+            <th width="100">작성일</th>
+            <th width="100">조회수</th>
+          </tr>
+          </thead>
+        <?php
+         if(isset($_GET['page'])){
+          $page = $_GET['page'];
+            }else{
+              $page = 1;
+            }
+              $sql = mq("select * from board");
+              $row_num = mysqli_num_rows($sql); //게시판 총 레코드 수
+              $list = 10; //한 페이지에 보여줄 개수
+              $block_ct = 5; //블록당 보여줄 페이지 개수
+
+              $block_num = ceil($page/$block_ct); // 현재 페이지 블록 구하기
+              $block_start = (($block_num - 1) * $block_ct) + 1; // 블록의 시작번호
+              $block_end = $block_start + $block_ct - 1; //블록 마지막 번호
+
+              $total_page = ceil($row_num / $list); // 페이징한 페이지 수 구하기
+              if($block_end > $total_page) $block_end = $total_page; //만약 블록의 마지박 번호가 페이지수보다 많다면 마지박번호는 페이지 수
+              $total_block = ceil($total_page/$block_ct); //블럭 총 개수
+              $start_num = ($page-1) * $list; //시작번호 (page-1)에서 $list를 곱한다.
+
+              $sql2 = mq("select * from board order by idx desc limit $start_num, $list");  
+              while($board = $sql2->fetch_array()){
+              $title=$board["title"]; 
+                if(strlen($title)>30)
+                { 
+                  $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
+                }
+                //$sql3 = mq("select * from reply where con_num='".$board['idx']."'");
+                //$rep_count = mysqli_num_rows($sql3);
+              ?>
+        <tbody>
+          <tr>
+          <td width="70"><?php echo $board['idx']; ?></td>
+          <td width="500"><?php 
+             $lockimg = "<img src='./img/lock.png' alt='lock' title='lock' with='20' height='20' />";
+            if($board['lock_post']=="1")
+            { ?>
+              <a href='./ck_read.php?idx=<?php echo $board["idx"];?>'><?php echo $title, $lockimg;
+            }else{  ?>
+        <a href='./read.php?idx=<?php echo $board["idx"]; ?>'><?php echo $title; }?><span class="re_ct"></span></a></td>
+          <td width="120"><?php echo $board['name']?></td>
+          <td width="100"><?php echo $board['date']?></td>
+          <td width="100"><?php echo $board['hit']; ?></td>
+        </tr>
+        </tbody>
+        <?php } ?>
+        </table>
+    
+        <!---페이징 넘버 --->
+        <div id="page_num">
+          <ul>
+          <?php
+          if($page <= 1)
+          { //만약 page가 1보다 크거나 같다면
+            echo "<li class='fo_re'><<</li>"; //처음이라는 글자에 빨간색 표시 
+          }else{
+            echo "<li><a href='?page=1'><<</a></li>"; //아니라면 처음글자에 1번페이지로 갈 수있게 링크
+          }
+          if($page <= 1)
+          { //만약 page가 1보다 크거나 같다면 빈값
+            
+          }else{
+          $pre = $page-1; //pre변수에 page-1을 해준다 만약 현재 페이지가 3인데 이전버튼을 누르면 2번페이지로 갈 수 있게 함
+            echo "<li><a href='?page=$pre'><</a></li>"; //이전글자에 pre변수를 링크한다. 이러면 이전버튼을 누를때마다 현재 페이지에서 -1하게 된다.
+          }
+          for($i=$block_start; $i<=$block_end; $i++){ 
+            //for문 반복문을 사용하여, 초기값을 블록의 시작번호를 조건으로 블록시작번호가 마지박블록보다 작거나 같을 때까지 $i를 반복시킨다
+            if($page == $i){ //만약 page가 $i와 같다면 
+              echo "<li class='fo_re'>[$i]</li>"; //현재 페이지에 해당하는 번호에 굵은 빨간색을 적용한다
+            }else{
+              echo "<li><a href='?page=$i'>[$i]</a></li>"; //아니라면 $i
+            }
+          }
+          if($block_num >= $total_block){ //만약 현재 블록이 블록 총개수보다 크거나 같다면 빈 값
+          }else{
+            $next = $page + 1; //next변수에 page + 1을 해준다.
+            echo "<li><a href='?page=$next'>></a></li>"; //다음글자에 next변수를 링크한다. 현재 4페이지에 있다면 +1하여 5페이지로 이동하게 된다.
+          }
+          if($page >= $total_page){ //만약 page가 페이지수보다 크거나 같다면
+            echo "<li class='fo_re'>>></li>"; //마지막 글자에 긁은 빨간색을 적용한다.
+          }else{
+            echo "<li><a href='?page=$total_page'>>></a></li>"; //아니라면 마지막글자에 total_page를 링크한다.
+          }
+          ?>
+          </ul>
+        </div>
+        <div id="write_btn">
+          <a href="./ck_write.php"><button>글쓰기</button></a>
+        </div>
+      </div>
     </div>
     <!-- /.row -->
-      </div>
-      <!-- /.row -->
-    </div>
-  </div>
-
 
   </div>
-  <!-- /.container -->
+  <!-- /.wrap -->
 
   <!-- Footer -->
   <footer class="py-4 bg-dark">
